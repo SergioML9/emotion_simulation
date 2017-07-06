d7de3983a6823fa447c95fcee96c13513040367f
Merinom <eduardo.merinom13@gmail.com>
2017-07-06 11:32:21 +0200

    Fixed bug movement

--- model/ramenScript.py
+++ model/ramenScript.py
@@ -6,11 +6,11 @@ for i in range(0, 10):
 	steps[0].append({ "agent": i, "position": "Hall.1"})
 
 def addAgentMovement(agent, room, stepStart, stepEnd):
-	steps[stepStart+1].append({"agent":agent.unique_id, "moveTo": room, "toStep": stepEnd})
+	steps[stepStart+1].append({"agent":agent.unique_id, "moveTo": room, "toStep": stepEnd+1})
 
 def generateJSON():
 	data = {"type":0, "steps": steps}
 	print(data)
-	with open('move.json', 'w') as outfile:
+	with open('lab_move.json', 'w') as outfile:
 		json.dump(data, outfile)
 		outfile.close()
